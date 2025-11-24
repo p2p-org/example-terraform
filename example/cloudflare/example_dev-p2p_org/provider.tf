a7b26d6bccd2b980e47fb1ed331d88a52f4f016c
Mikhail Nacharov <1246847+mnacharov@users.noreply.github.com>
2025-11-24 11:19:14 +0100

    init example.dev-p2p.org cloudflare workspace

---
+++ example/cloudflare/example_dev-p2p_org/provider.tf
@@ -0,0 +1,39 @@
+terraform {
+  cloud {
+    hostname     = "tap-api.infra.p2p.org"
+    organization = "example"
+
+    workspaces {
+      name = "cloudflare-example_dev-p2p_org"
+    }
+  }
+  required_version = ">= 1.10.5, < 2"
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 5.4.0, < 6"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 5.12.0, < 6"
+    }
+  }
+}
+
+provider "vault" {
+  skip_child_token = true
+}
+
+ephemeral "vault_kv_secret_v2" "cloudflare_zone_creds" {
+  mount    = "iaas/kv/default"
+  name     = "terrakube/example/cloudflare-example_dev-p2p_org"
+  mount_id = "1"
+}
+
+provider "cloudflare" {
+  api_token = tostring(ephemeral.vault_kv_secret_v2.cloudflare_zone_creds.data.key)
+}
+
+data "cloudflare_zone" "example_dev-p2p_org" {
+  zone_id = "4244bead163c3a48dba41b3f36f214db" # example.dev-p2p.org
+}
