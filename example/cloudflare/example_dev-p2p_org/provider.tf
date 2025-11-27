db16e734e3a5056a64eaa6875a7b197baf605c36
Mikhail Nacharov <1246847+mnacharov@users.noreply.github.com>
2025-11-27 10:26:46 +0100

    fix(vault): use ephemeral to get cloudflare api key

--- example/cloudflare/example_dev-p2p_org/provider.tf
+++ example/cloudflare/example_dev-p2p_org/provider.tf
@@ -4,11 +4,15 @@ terraform {
     organization = "example"
 
     workspaces {
-      name = "cloudflare-example_dev-p2p_org"
+      name = "example-terraform"
     }
   }
   required_version = ">= 1.10.5, < 2"
   required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 5.4.0, < 6"
+    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = ">= 5.12.0, < 6"
@@ -16,22 +20,25 @@ terraform {
   }
 }
 
+provider "vault" {
+  skip_child_token = true
+}
 
-variable "cloudflare_api_token" {
-  type    = string
-  default = ""
+ephemeral "vault_kv_secret_v2" "cloudflare_zone_creds" {
+  mount    = "iaas/kv/default"
+  name     = "terrakube/example/cloudflare-example_dev-p2p_org"
+  mount_id = "1"
 }
 
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  api_token = tostring(ephemeral.vault_kv_secret_v2.cloudflare_zone_creds.data.key)
 }
 
 data "cloudflare_zone" "example_dev-p2p_org" {
   zone_id = "4244bead163c3a48dba41b3f36f214db" # example.dev-p2p.org
 }
 
-output "cloudlfare_key" {
-  description = "Reveal Cloudflare Key"
-  value       = nonsensitive(var.cloudflare_api_token)
-  sensitive   = false
+output "cloudlfare_zone_info" {
+  description = "Test cloudflare connection"
+  value       = data.cloudflare_zone.example_dev-p2p_org.name
 }
